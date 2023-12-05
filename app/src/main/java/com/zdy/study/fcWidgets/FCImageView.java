95aaddd5c81b8de9541504a6bea3ac20789411eb
liuhongtian <771440666@qq.com>
2023-12-05 20:20:22 +0800

    学习助手

---
+++ app/src/main/java/com/zdy/study/fcWidgets/FCImageView.java
@@ -0,0 +1,58 @@
+package com.zdy.study.fcWidgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.view.ViewCompat;
+
+
+@SuppressLint("AppCompatCustomView")
+public class FCImageView extends ImageView {
+
+    public FCImageView(Context context) {
+        super(context);
+        setFocusChange();
+    }
+
+    public FCImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusChange();
+    }
+
+    public FCImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setFocusChange();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public FCImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setFocusChange();
+    }
+
+    private void setFocusChange(){
+        this.setOnFocusChangeListener((view, b) -> {
+            if (b) {
+                // 此处为得到焦点时的处理内容
+                ViewCompat.animate(view)
+                        .scaleX(1.10f)
+                        .scaleY(1.10f)
+                        .translationZ(1)
+                        .start();
+
+            } else {
+                // 此处为失去焦点时的处理内容
+                ViewCompat.animate(view)
+                        .scaleX(1)
+                        .scaleY(1)
+                        .translationZ(1)
+                        .start();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/zdy/study/fragments/MainFragment.java
@@ -0,0 +1,34 @@
+package com.zdy.study.fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.askia.common.base.BaseFragment;
+import com.zdy.study.databinding.FragmentMainBinding;
+
+public class MainFragment extends BaseFragment {
+    FragmentMainBinding mFragmentMainBinding;
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onInitViewModel() {
+
+    }
+
+    @Override
+    public View onInitDataBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        return null;
+    }
+
+    @Override
+    public void onSubscribeViewModel() {
+
+    }
+}
